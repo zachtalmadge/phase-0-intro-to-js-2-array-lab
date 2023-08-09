a05b996518ecb2d8f5c7cf75c4e996a05c5cc489
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-08-09 15:43:54 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    return [...cats, name]
+}
+
+function removeFirstCat(){
+    let copy = [...cats];
+    copy.shift();
+    return copy;
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    let copy = [...cats];
+    copy.pop();
+    return copy;
+}
